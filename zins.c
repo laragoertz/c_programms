18b678cbabe45183cb0f42e132b3b1a8a8bf6fb8
Lara <58141493+laragoertz@users.noreply.github.com>
2020-01-31 19:15:45 +0100

    Update and rename zins to zins.c

--- zins.c
+++ zins.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
+void rechnen(double Zins, double AB, double AD)
+{
+    printf("das ist das unterprogramm");
+    double c=0.0;
+do
+{
+    AB=AB*(1+Zins);
+}
+while (c<AD,c++);
+    printf("\nDer Anlagebetrag ist %lf\n",AB);
+}
+
+
 int main()
 {
     double Zins;
     double AB;
     double AD;
-    double f;
     printf("\nGeben Sie den Zins ein\n");
     scanf("%lf", &Zins);
     printf("\nGeben Sie den Anlagebetrag ein\n");
     scanf("%lf", &AB);
     printf("\nGeben Sie die Anlagedauer ein\n");
     scanf("%lf", &AD);
-    f=rechnen(Zins,AB,AD);
+    rechnen(Zins,AB,AD);
     
-}
-  
-      
-int rechnen(double Zins, double AB, double AD){
-    printf("das ist das unterprogramm");
-    double c=0.0;
-do{
-    AB=AB*(1+Zins);
-}
-     while (c<AD);
-    printf("Der Anlagebetrag ist %lf\n",AB);
-    return c;
 }
   
   
